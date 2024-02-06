9e9e4d05a2ed8bc3aa374e3fc31f68637d9b3b03
Ashutosh Kapoor <109470397+SteffanSingh@users.noreply.github.com>
2024-02-06 19:49:12 +0100

    Sql Alchemy

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
-from flask import Flask, jsonify,render_template,request,redirect, url_for
-from data_managers.data_manager_interface_json import JSONDataManager
-import  json
-import  requests
+from data_managers.data_manager_interface_json import SQLiteDataManager
+from flask import Flask,  render_template, request,redirect,url_for
+from MovieWeb_app.data_managers.data_models import db, User, Movie
+import  requests, json
 
-
-movie_data = "data/data.json"
+#movie_data = "data/data.json"
 
 
 app = Flask(__name__)
 
-data_manager = JSONDataManager(movie_data)
+data_manager = SQLiteDataManager('moviwebapp.sqlite')
 
 
-def read_data():
-    with open("data/data.json", "r") as fileObject:
-        data = json.loads(fileObject.read())
-    return data
 
 
 @app.route('/')
@@ -38,41 +33,31 @@ def user_movie_list(user_id):
     user_movies = data_manager.get_user_movies(user_id)
     user_list = data_manager.get_all_users()
     user_id_list = data_manager.get_all_users_id()
-    index = user_id_list.index(user_id)
-    name = user_list[index]
-    return render_template("favourite_movie.html",user_name =name, movies = user_movies, user_id=user_id)
+    #index = user_id_list.index(user_id)
+    name = db.session.query(User).filter(User.id == user_id).one()
+    return render_template("favourite_movie.html",user_name = name, movies = user_movies, user_id = user_id)
 
 
 @app.route("/add_user", methods = ["GET","POST"])
 def add_user():
     """function to implement the add user in the given user data."""
     if request.method == "POST":
-        users = read_data()
+        users = db.session.query(User).all()
         name = request.form['name']
         if name == None:
             return render_template("users.html")
         else:
-            users_id_list = data_manager.get_all_users_id()
-            user = {}
-            id = max(users_id_list) + 1
-            user['name'] = name
-            users[str(id)] = user
-            user["movies"]=[]
-            user_list = data_manager.get_all_users()
-            user_list.append(name)
-            with open("data/data.json", "w") as fileObject:
-                fileObject.write(json.dumps(users, indent=4))
+            data_manager.add_user(name)
+            users = db.session.query(User).all()
             return redirect(url_for("list_users"))
     return render_template("add_user.html")
 
 @app.route("/delete_user/<int:user_id>", methods=["GET","DELETE"])
 def delete_user(user_id):
     """function to implement to delete user from a given user list."""
-    all_user_data = read_data()
-    if str(user_id) in list(all_user_data.keys()):
-        all_user_data.pop(str(user_id))
-    with open("data/data.json", "w") as fileObject:
-        fileObject.write(json.dumps(all_user_data, indent=4))
+    all_user_data = data_manager.get_all_users()
+    db.session.query(User).filter(User.id == user_id).delete()
+    db.session.commit()
     return redirect(url_for("list_users"))
 
 
@@ -82,15 +67,12 @@ def add_user_movie(user_id):
     users_list = data_manager.get_all_users()
     users_id_list = data_manager.get_all_users_id()
     index = users_id_list.index(user_id)
-    user_name = users_list[index]
-    users = read_data()
-    user_movie_list = data_manager.get_user_movies(user_id)
-    user = {}
-    movie = {}
-    movie_id_list = []
+    user_name = db.session.query(User).filter(User.id == user_id).name
 
-    for movie in user_movie_list:
-        movie_id_list.append(movie["movie_id"])
+    user_movie_list = data_manager.get_user_movies(user_id)
+
+
+
 
     if request.method == "POST":
         name = request.form["name"]
@@ -99,29 +81,36 @@ def add_user_movie(user_id):
         res = requests.get(Url)
         data = res.json()
         if data['Response'] == "False":
-            return render_template("movie_not_found.html", user_name=user_name,user_id=user_id)
+            return render_template("movie_not_found.html", user_name = user_name, user_id = user_id)
         for movie in user_movie_list:
-            if movie["movie_name"] == data["Title"]:
+            if movie  == data["Title"]:
                 return render_template("movie_already_exists.html", user_name=user_name, user_id=user_id)
-        movie["movie_name"] = data["Title"]
-        movie["rating"] = data["imdbRating"]
-        movie["year"] = data["Year"]
-        movie["director"] = data["Director"]
-        movie["movie_id"] = max(movie_id_list) + 1
-        movie["poster"]= data["Poster"]
-        movie["note"]=[]
+        movie_name  = data["Title"]
+        rating  = data["imdbRating"]
+        year  = data["Year"]
+        director  = data["Director"]
+
+        poster  = data["Poster"]
+        note  = " "
+        movie = Movie(
+            movie_name = name,
+            director = director,
+            rating = rating,
+            year  = year,
+            poster = poster,
+            note = note
+        )
+
 
 
         for user in users_list:
             if user_id not in users_id_list:
                 return render_template("users.html")
             elif user_id in users_id_list:
-                users[str(user_id)]["movies"].append(movie)
-                with open("data/data.json", "w") as fileObject:
-                    fileObject.write(json.dumps(users, indent=4))
+                data_manager.add_movie(movie)
+
                 return redirect(url_for("user_movie_list",user_id=user_id))
     return render_template("add_movie.html", user_id=user_id)
-
 
 
 @app.route("/users/<int:user_id>/update_movie/<int:movie_id>", methods=["GET", "POST"])
@@ -131,21 +120,20 @@ def update_movie(user_id, movie_id):
     users_list = data_manager.get_all_users()
     users_id_list = data_manager.get_all_users_id()
     index = users_id_list.index(user_id)
-    user_name = users_list[index]
-    users = read_data()
+    user_name = db.session.query(User).filter(User.id == user_id).name
+
+    #movies = data_manager.get_user_movies_name_list(user_id)
     user_movie_list = data_manager.get_user_movies(user_id)
+    movie_to_update = db.session.query(Movie).filter(Movie.user_id == user_id).one()
     if request.method == "POST":
         rating = request.form["rating"]
         note = request.form["note"]
-        for index,movie in enumerate(user_movie_list):
+        for index, movie in enumerate(user_movie_list):
             if int(movie["movie_id"]) == movie_id:
-                movie["rating"] = rating
-                movie["note"] = note
-                users[str(user_id)]["movies"][index].update(movie)
-
-                with open("data/data.json", "w") as fileObject:
-                    fileObject.write(json.dumps(users, indent=4))
-                return render_template("favourite_movie.html", user_name=user_name, user_id=user_id,movie_id=movie_id, movies=user_movie_list)
+                movie_to_update.rating = rating
+                movie_to_update.note  = note
+                db.session.commit()
+                return render_template("favourite_movie.html", user_name=user_name, user_id=user_id,movie_id=movie_id, movies = user_movie_list)
     return render_template("update.html", user_name=user_name,movie_id=movie_id, movies=user_movie_list, user_id=user_id)
 
 
@@ -155,18 +143,15 @@ def delete_movie(user_id, movie_id):
 
     users_list = data_manager.get_all_users()
     users_id_list = data_manager.get_all_users_id()
-    index = users_id_list.index(user_id)
-    user_name = users_list[index]
-    users = read_data()
+    user_name = db.session.query(User).filter(User.id == user_id).name
     user_movie_list = data_manager.get_user_movies(user_id)
     for movie in user_movie_list:
-        if int(movie["movie_id"]) == movie_id:
-            users[str(user_id)]["movies"].remove(movie)
-            with open("data/data.json", "w") as fileObject:
-                fileObject.write(json.dumps(users, indent=4))
-            return redirect(url_for("user_movie_list",user_id=user_id))
+        if int(movie.movie_id) == movie_id:
+            db.session.query(Movie).filter(Movie.user_id == user_id)
 
-    return render_template("favourite_movie.html", user_name=user_name, movies=user_movie_list, user_id=user_id)
+            return redirect(url_for("user_movie_list", user_id = user_id))
+
+    return render_template("favourite_movie.html", user_name = user_name, movies = user_movie_list, user_id = user_id)
 
 
 
---
+++ data_managers/data_manager_interface_json.py
@@ -0,0 +1,98 @@
+import json
+import os
+from .data_manager_interface import DataManagerInterface
+
+from flask_sqlalchemy import  SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from flask import  Flask
+from MovieWeb_app.data_managers.data_models import User, Movie
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy()
+Base = declarative_base()
+
+engine = create_engine('sqlite:///moviwebapp.sqlite')
+Base.metadata.create_all(engine)
+
+# Create a database session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///moviwebapp.sqlite'
+db.init_app(app)
+
+# Define the path to the data.json file
+data_file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'data.json')
+
+
+def file_data(filename):
+    with open(filename, "r") as fileObject:
+        json_data = fileObject.read()
+        data = json.loads(json_data)
+    return data
+
+class SQLiteDataManager(DataManagerInterface):
+    def __init__(self, filename):
+        self.db = SQLAlchemy(filename)
+
+    def get_all_users(self):
+        # Return all the users all users
+        users_data = self.db.session.query(User).all()
+        users_list = [user.name for user in users_data]
+        return users_list
+
+    def get_all_users_id(self):
+        users_data = self.db.session.query(User).all()
+        users_id_list = [user.id for user in users_data]
+        return users_id_list
+
+    def get_all_movies_id(self, user_id):
+        movies_data = self.db.session.query(Movie).filter(Movie.user_id == user_id).all()
+        user_movies_id_list = [movie.id for movie in movies_data]
+
+        return user_movies_id_list
+
+
+    def get_user_movies(self, user_id):
+        # Return all the movies for a given user
+
+        movies_data = self.db.session.query(Movie).filter(Movie.user_id == user_id).all()
+        #user_movies_list = [movie.movie_name for movie in movies_data]
+        return movies_data
+
+    def get_user_movies_name_list(self, user_id):
+        movies_data = self.db.session.query(Movie).filter(Movie.user_id == user_id).all()
+        user_movies_list = [movie.movie_name for movie in movies_data]
+        return user_movies_list
+
+    def add_user(self, user):
+
+        self.db.session.add(user)
+        self.db.session.commit()
+
+    def add_movie(self, movie):
+
+        self.db.session.add(movie)
+        self.db.session.commit()
+
+    #def update_movie(self, movie ):
+     #   movie_to_update = session.query(Movie).filter(Movie.movie_name == movie_name).one()
+      #  #movie_to_update.rating = new_rating
+       # session.commit()
+
+
+    #def delete_movie(movie_id):
+     #   pass
+
+
+#obj = SQLiteDataManager(data_file_path)
+#x=obj.get_all_movies_id("")
+#print(x)
+
+#with app.app_context():
+ #   db.create_all()
